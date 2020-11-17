323f0ffdd4179b789891a51d63c6e52b8387e260
yiyb0603 <yiyb0603@gmail.com>
2020-11-17 13:59:00 +0900

    [ADD] progress bars

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,9 +1,11 @@
 import React, { CSSProperties } from 'react';
 import classNames from 'classnames';
 import { ClassNamesFn } from 'classnames/types';
-import { FaHtml5, FaCss3Alt, FaJava, FaReact, FaSass } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaJava, FaReact, FaSass, FaVuejs } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io';
+import { GrNode } from 'react-icons/gr';
 import StackItem from './StackItem';
+import NextLogo from 'assets/images/Next.png';
 
 const style = require("./AboutMe.scss");
 const cx: ClassNamesFn = classNames.bind(style);
@@ -11,11 +13,14 @@ const cx: ClassNamesFn = classNames.bind(style);
 const AboutMe = () => {
     const C_LOGO: string = "https://vo.la/7hyjC";
     const TS_LOGO: string = "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg";
+    const MOBX_LOGO: string = "https://miro.medium.com/max/400/1*nvHO6KuxX5B6FVXZ_hqTZw.png";
+    const TYPE_ORM_LOGO: string = "https://avatars2.githubusercontent.com/u/20165699?s=400&v=4";
 
     const imageStyle: CSSProperties = {
         width: 50,
         height: 50,
-    }
+        borderRadius: 5,
+    };
 
     return (
         <div className ="AboutMe">
@@ -46,8 +51,28 @@ const AboutMe = () => {
                     <FaReact className={cx('StackItem-Wrapper-Icon-React')} />
                 </StackItem>
 
+                <StackItem progressName="MobX">
+                    <img src ={MOBX_LOGO} alt ="mobx" style={imageStyle} />
+                </StackItem>
+
                 <StackItem progressName="Sass">
                     <FaSass className={cx('StackItem-Wrapper-Icon-Sass')} />
+                </StackItem>
+
+                <StackItem progressName="Vue">
+                    <FaVuejs className={cx('StackItem-Wrapper-Icon-Vue')} />
+                </StackItem>
+
+                <StackItem progressName="Next">
+                    <img src={NextLogo} alt ="nextjs" style ={imageStyle} />
+                </StackItem>
+
+                <StackItem progressName="Express">
+                    <GrNode className={cx('StackItem-Wrapper-Icon-Express')} />
+                </StackItem>
+
+                <StackItem progressName="TypeORM">
+                    <img src ={TYPE_ORM_LOGO} alt ="typeorm" style ={imageStyle} />
                 </StackItem>
             </div>
         </div>

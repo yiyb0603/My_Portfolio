b937fe7e66822c15f967837df0e14829539c1409
권용빈 <yiyb0603@gmail.com>
2020-12-06 18:35:21 +0900

    Updates

--- src/enum/HistoryEnum.ts
+++
@@ -1,6 +0,0 @@
-export enum HistoryEnums {
-  'AWARDS',
-  'ACTIVITY',
-  'COMPETITION',
-  'CONFERENCE',
-}
--- src/pages/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AboutMe} from './AboutMePage';
-export { default as History } from './HistoryPage';
-export { default as Project } from './ProjectsPage';

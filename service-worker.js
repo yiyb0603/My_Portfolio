28a4508bc0bc6ed7d6e9e0595d33a9e643c4e87e
yiyb0603 <yiyb0603@gmail.com>
2020-12-08 08:57:50 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_portfolio/precache-manifest.602d838b0c1aecd6f8b6a3166fa01c48.js"
+  "/my_portfolio/precache-manifest.e1cf9eecde0b15eb2f13e7456c3459b2.js"
 );
 
 self.addEventListener('message', (event) => {

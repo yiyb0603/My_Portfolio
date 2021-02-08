7517c31942a08eb3837175e04cc5ab7610ac7c41
yiyb0603 <yiyb0603@gmail.com>
2021-02-09 00:54:08 +0900

    Fix: readonly, fontsize

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -1,11 +1,11 @@
 class Constants {
-  public static GITHUB_ADDRESS: string = 'http://github.com/yiyb0603';
-  public static FACEBOOK_ADDRESS: string = 'https://www.facebook.com/profile.php?id=100010933171210';
-  public static BLOG_ADDRESS: string = 'https://velog.io/@yiyb0603';
-  public static ROCKET_ADDRESS: string = 'https://www.rocketpunch.com/@513886f28e2b4088';
-
-  public static PHONE_NUMBER: string = '010-8852-5110';
-  public static EMAIL_ADDRESS: string = 'yiyb0603@gmail.com';
+  public static readonly GITHUB_ADDRESS: string = 'http://github.com/yiyb0603';
+  public static readonly FACEBOOK_ADDRESS: string = 'https://www.facebook.com/profile.php?id=100010933171210';
+  public static readonly BLOG_ADDRESS: string = 'https://velog.io/@yiyb0603';
+  public static readonly ROCKET_ADDRESS: string = 'https://www.rocketpunch.com/@513886f28e2b4088';
+  
+  public static readonly PHONE_NUMBER: string = '010-8852-5110';
+  public static readonly EMAIL_ADDRESS: string = 'yiyb0603@gmail.com';
 };
 
 export default Constants;

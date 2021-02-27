ad467e91bb0d027dfce1f83a8e95458fc40ef088
yiyb0603 <yiyb0603@gmail.com>
2021-02-27 14:40:00 +0900

    Delete: move func

--- src/components/Common/ScrollProgress/ScrollProgress.tsx
+++ src/components/Common/ScrollProgress/ScrollProgress.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, useRef, memo, MouseEvent } from 'react';
+import React, { useState, useCallback, useEffect, useRef, memo } from 'react';
 import classNames from 'classnames';
 import { ClassNamesFn } from 'classnames/types';
 
@@ -8,26 +8,6 @@ const cx: ClassNamesFn = classNames.bind(style);
 const ScrollProgress = memo((): JSX.Element => {
   const [width, setWidth] = useState<number>(0);
   const progressRef = useRef<HTMLDivElement | null>(null);
-
-  const handleProgressMove = useCallback((e: MouseEvent<HTMLDivElement>): void => {
-    if (progressRef.current !== null) {
-      const { scrollWidth } = progressRef.current;
-      const { clientX } = e;
-
-      const selectedPercent: number = (clientX / scrollWidth) * 100;
-      setWidth(selectedPercent);
-      
-      const { innerHeight } = window;
-      const { scrollHeight } = document.body;
-
-      const moveScrollPercent: number = ((scrollHeight * selectedPercent) / 100) - innerHeight;
-
-      window.scrollTo({
-        top: moveScrollPercent,
-        behavior: 'smooth',
-      })
-    }
-  }, []);
 
   const handleScroll = useCallback((): void => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
@@ -52,7 +32,7 @@ const ScrollProgress = memo((): JSX.Element => {
   }, [handleScroll]);
 
   return (
-    <div className={cx('ScrollProgress')} ref={progressRef} onClick={handleProgressMove}>
+    <div className={cx('ScrollProgress')} ref={progressRef}>
       <div className={cx('ScrollProgress-Progress')} style={{ transform: `scale(${width}, 1)` }} ></div>
     </div>
   );

c4be7337ac3dd527e5ac06ded834c2382453b0a4
yiyb0603 <yiyb0603@gmail.com>
2021-03-14 17:54:29 +0900

    Fix: responsive ui
    반응형 ui 수정

--- src/components/AboutMe/Certificate/Certificate.tsx
+++ src/components/AboutMe/Certificate/Certificate.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useCallback } from 'react';
 import classNames from 'classnames';
 import { ClassNamesFn } from 'classnames/types';
 import { FaRegIdBadge } from 'react-icons/fa';
@@ -9,13 +9,17 @@ const style = require('./Certificate.scss');
 const cx: ClassNamesFn = classNames.bind(style);
 
 const Certificate = (): JSX.Element => {
+  const handlePushToLink = useCallback((link: string): void => {
+    window.open(link);
+  }, []);
+
   return (
     <div className={cx('Certificate')}>
       <SectionTitle name='자격증' icon={<FaRegIdBadge />} />
 
       {
         certificates.map((certificate: ICertificate, idx: number) => {
-          const { name, color, certifiedAt } = certificate;
+          const { name, color, certifiedAt, link } = certificate;
 
           const colorBar: CSSProperties = {
             width: 5,
@@ -28,7 +32,12 @@ const Certificate = (): JSX.Element => {
             <div className={cx('Certificate-CertifiWrap')} key={idx}>
               <div className={cx('Certificate-CertifiWrap-Left')}>
                 <div style={colorBar}></div>
-                <div>{name}</div>
+                <div
+                  className={cx('Certificate-CertifiWrap-Left-Name')}
+                  onClick={() => handlePushToLink(link)}
+                >
+                  {name}
+                </div>
               </div>
 
               <div className={cx('Certificate-CertifiWrap-Date')}>{certifiedAt}</div>
--- src/data/Certificate.ts
+++ src/data/Certificate.ts
@@ -4,6 +4,7 @@ export interface ICertificate {
   name: string;
   color: string;
   certifiedAt: string | Date;
+  link: string;
 }
 
 export const certificates: ICertificate[] = [
@@ -11,5 +12,6 @@ export const certificates: ICertificate[] = [
     name: 'OCAJP 오라클 국제 자격증',
     color: Palette.red,
     certifiedAt: '2021-01-08',
+    link: 'https://velog.io/@yiyb0603/OCAJP-%EC%9E%90%EA%B2%A9%EC%A6%9D-%EC%8B%9C%ED%97%98-%ED%9B%84%EA%B8%B0',
   },
 ];
